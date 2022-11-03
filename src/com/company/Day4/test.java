79461a9bfd204b1e91208fc743963483018d5104
HarshaKarkera <harshakarkera97@gmail.com>
2022-11-03 11:35:29 -0400

    newFeatureChanges

--- src/com/company/Day4/test.java
+++ src/com/company/Day4/test.java
@@ -5,7 +5,11 @@ public class test
     public static void main(String[] args)
     {
         String str="namdam";
-        if(str.length()>1);
+
+        System.out.println(  (str.substring(str.length()-1,str.length())).concat(str.substring(1,str.length()-1)).concat(str.substring(0,1)));
+
+
+
 
 
     }
